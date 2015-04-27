7372c019c126a3819d75868e07125d7c87449068
dminer78 <dminer78@users.noreply.github.com>
2015-04-27 16:21:21 -0700

    Changed to 2CubedBot for dminer78.com domain

---
+++ contactForm.php
@@ -0,0 +1,36 @@
+<?php
+
+/* Subject and Email Vars  */
+
+	$emailSubject = '2CubedBot - Help Form!';
+	$emailAddress = 'dminer789@gmail.com';
+	
+/* Set Vars to Info */
+
+	$nameInput = $_POST['name'];
+	$emailInput = $_POST['email'];
+	$messageInput = $_POST['message'];
+	
+	$body = <<<EOD
+<br /><hr><br />
+Name: $name <br />
+Email: $email <br />
+Message: $message
+EOD;
+
+	$headers = "From: $email\r\n";
+	$headers .= "Content-type: text/html\r\n";
+	$success = mail($emailAddress, $emailSubject, $body, $headers);
+	
+/* Alert user that email is sent */
+
+	$alert = <<<EOD
+<html>
+<style>
+alert("The form has been sent!");
+</style>
+</html>
+EOD;
+echo "$alert";
+	
+?>
