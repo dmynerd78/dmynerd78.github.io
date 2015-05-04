e60cf7053b4ee8cc74496f8c3a313308d862613c
dminer78 <dminer78@users.noreply.github.com>
2015-05-04 12:43:05 -0700

    Added copyright js file to everything. Other misc things

--- js/copyright.js
+++ js/copyright.js
@@ -9,4 +9,5 @@ Conversation between me and a few other people.
 Have inside jokes such as dhannah and cake and food
 make it interactive. Multiple options available for options you do
 Use konami code to start it off?
+Yaaaay easter eggs!
 */
