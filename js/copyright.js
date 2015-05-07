09935101e5c4fe2ef9747a74592a70a87dcc805b
dminer78 <dminer78@users.noreply.github.com>
2015-05-06 19:01:46 -0700

    Analytics and easter eggs

--- js/copyright.js
+++ js/copyright.js
@@ -1,10 +1,24 @@
-console.log("~~~~~~~~~~~~~~~~~~~~~~~")
+console.log("%c~~~~~~~~~~~~~~~~~~~~~~~~~", "color: #BBC0C2;")
 
-console.log("(c) dminer78 <INSERT LOGO ASCII HERE />");
+console.log("%c(c) dminer78", "color: #FB9C10;");
+console.log("%c<INSERT LOGO ASCII HERE />", "color: red;")
 
-console.log("~~~~~~~~~~~~~~~~~~~~~~~")
+console.log("%c~~~~~~~~~~~~~~~~~~~~~~~~~", "color: #BBC0C2;")
+
+var food = function(answer) {
+	if (answer === undefined) {
+		options = ["FINE THEN! No food for you!!!", "You want food too? YAY! :D"]
+		console.log("Do you want food? Answer with 'food(1)' if you do! Do 'food(0)' if you don't want some!")
+	} else {
+		console.log(options[answer]);
+	}
+		
+}
+
 
 /*
+For 2CubedBot add some kind of bot in console
+
 Conversation between me and a few other people. 
 Have inside jokes such as dhannah and cake and food
 make it interactive. Multiple options available for options you do
