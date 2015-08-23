7cf848aad407552c6b84614106d6dc45e2bcd971
dminer78 <dminer78@users.noreply.github.com>
2015-08-23 05:03:20 +0000

    screen width detection is weird

--- js/css.js
+++ js/css.js
@@ -1,8 +1,10 @@
-if(window.innerWidth <= 700 || window.innerHeight <= 700 || screen.width <= 700 || screen.height <= 700) {
+if(screen.width <= 700 || screen.height <= 700) {
     alert("Mobile site is still under construction. Sorry for the inconvenience");
     $('head').append('<link rel="stylesheet" href="css/style-mobile.css" type="text/css" />');
     console.log("MOBILE css sheet appended");
+    mobile = true
 } else {
     $('head').append('<link rel="stylesheet" href="css/style-desktop.css" type="text/css" />');
     console.log("DESKTOP css sheet appended");
+    mobile = false
 }
