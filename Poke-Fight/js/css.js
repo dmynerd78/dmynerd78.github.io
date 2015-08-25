e9c510d5f984e2574d4890d8998c0bf12c2572d4
dminer78 <dminer78@users.noreply.github.com>
2015-08-25 02:53:07 +0000

    Cleaned up js and added CUSTOM BANNERgit add . \o/

---
+++ Poke-Fight/js/css.js
@@ -0,0 +1,18 @@
+if(screen.width <= 700 || screen.height <= 700) {
+    
+    alert("Mobile site is still under construction. Sorry for the inconvenience");
+    $('head').append('<link rel="stylesheet" href="css/style-mobile.css" type="text/css" />');
+    console.log("MOBILE css sheet appended");
+    mobile = true
+    
+} else {
+    // http://nicescroll.areaaperta.com/
+    $(document).ready(
+        function() {
+            $("html").niceScroll({cursorcolor:"#000", cursoropacitymax:"0.5", zindex: "9000"});
+        }
+    );
+    $('head').append('<link rel="stylesheet" href="css/style-desktop.css" type="text/css" />');
+    console.log("DESKTOP css sheet appended");
+    mobile = false
+}
