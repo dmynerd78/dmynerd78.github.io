fc962770ac5403887943cefc06c20504af09159b
dminer78 <dminer78@users.noreply.github.com>
2015-08-29 15:05:44 -0700

    MOAR IMAGES!!!

--- Poke-Fight/js/bg.js
+++ Poke-Fight/js/bg.js
@@ -8,7 +8,7 @@ if(screen.width <= 700 || screen.height <= 700) {
     var size = "desktop/";
 }
     
-var images = ['1.jpg', '2.jpg', '3.jpg', '4.jpg', '5.jpg', '6.jpg', '7.jpg', '8.jpg', ];
+var images = ['1.jpg', '2.jpg', '3.jpg', '4.jpg', '5.jpg', '6.jpg', '7.jpg', '8.jpg', '9.jpg', '10.jpg', ];
 
 var imageRandom = Math.floor(Math.random() * images.length)
 
