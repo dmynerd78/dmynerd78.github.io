6d9f0207afbae13ea80045f58b77fedb5c3ac756
dminer78 <dminer78@users.noreply.github.com>
2015-08-30 15:55:33 -0700

    Changed background images to be less obtrusive

--- Poke-Fight/js/bg.js
+++ Poke-Fight/js/bg.js
@@ -3,9 +3,9 @@
 
 /* TODO Change image sizes for more bandwidth friendliness */
 if(screen.width <= 700 || screen.height <= 700) {
-    var size = "mobile/";
+    var dir = "mobile/";
 } else {
-    var size = "desktop/";
+    var dir = "desktop/";
 }
     
 var images = ['1.jpg', '2.jpg', '3.jpg', '4.jpg', '5.jpg', '6.jpg', '7.jpg', '8.jpg', '9.jpg', '10.jpg', ];
@@ -13,4 +13,4 @@ var images = ['1.jpg', '2.jpg', '3.jpg', '4.jpg', '5.jpg', '6.jpg', '7.jpg', '8.
 var imageRandom = Math.floor(Math.random() * images.length)
 
 console.log("Using Image " + imageRandom + "as background")
-$('html').css({'background-image': 'url(images/bg/' + size + images[imageRandom] + ')'});
+$('html').css({'background-image': 'url(images/bg/' + dir + 'overlay.png), url(images/bg/' + dir + images[imageRandom] + ')'});
