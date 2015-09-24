524d668b767f1021f309ff935cd10b75d087a016
dminer78 <dminer78@users.noreply.github.com>
2015-09-24 17:29:57 +0000

    'Working' city challenge site

---
+++ city_challenge/js/css.js
@@ -0,0 +1,10 @@
+if(screen.width <= 700 || screen.height <= 700) {
+    alert("Mobile site is still under construction. Sorry for the inconvenience");
+    $('head').append('<link rel="stylesheet" href="css/style-mobile.css" type="text/css" />');
+    console.log("MOBILE css sheet appended");
+    var screen = "MOBILE";
+} else {
+    $('head').append('<link rel="stylesheet" href="css/style-desktop.css" type="text/css" />');
+    console.log("DESKTOP css sheet appended");
+    var screen = "DESKTOP";
+}
---
+++ city_challenge/js/getVideos.js
@@ -0,0 +1,32 @@
+// https://console.developers.google.com/project/youtube-php-1003/apiui/credential
+
+videoObject = {};
+$.getJSON("https://www.googleapis.com/youtube/v3/playlistItems?part=snippet&maxResults=3&playlistId=PLpsRQ_00A2YQTabJg3IoLBW62HTnXCH8S&key=AIzaSyB6wccXxgBeO6VJ5EHV-zTrBvWTKZE8H0k", function(data) {
+$.each(data.items, function(i, val) {
+        i += 1;
+        videoObject[i] = {"title":val.snippet.title, "id":val.snippet.resourceId.videoId};
+        console.log(videoObject[i]);
+ });
+});
+
+var appendVideos = function() {
+    for(i = 1; i <= 3; i++) {
+        document.getElementById(i).innerHTML = "<a href='https://www.youtube.com/watch?v=" + 
+                                                (videoObject[i]["id"]) + 
+                                                " ' target='_blank'><img class='thumbnail' src='https://i.ytimg.com/vi/"+ 
+                                                (videoObject[i]["id"]) + 
+                                                "/mqdefault.jpg' /></a>";
+    }
+};
+
+var detectVideos = function() {
+    if(document.getElementById(1).innerHTML === "") {
+        console.log("Unable to load vidoes reloading.")
+        //location.reload();
+    }
+};
+
+setTimeout(function(){
+    appendVideos();
+    detectVideos();
+}, 1000);
