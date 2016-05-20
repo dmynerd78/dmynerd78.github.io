478acad668412bb628c1cea0d8c7cde2aefd6244
dminer78 <benzbeth@gmail.com>
2016-05-20 08:30:56 -0700

    misc updates

--- services/streams/index.php
+++
@@ -1,8 +0,0 @@
-<?php
-    $files = scandir('.');
-    foreach($files as $file) {
-        if (strlen($file) > 3 && $file != basename($_SERVER['PHP_SELF'])) {
-          echo "<a href=", $file, ">", $file, "</a>\n";
-        }
-    }
-?> 
