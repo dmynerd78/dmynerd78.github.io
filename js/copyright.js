ff6c22cd2153f3edeec5f8b78bfa597d2775b750
dmynerd78 <dmynerd78@users.noreply.github.com>
2020-02-29 14:50:24 -0700

    Updating template along with creating
    Now with support for articles (in case I ever have the time to make some)

--- js/copyright.js
+++ js/copyright.js
@@ -4,13 +4,3 @@ console.log("%c(c) dmynerd78", "color: #FB9C10;");
 console.log("%c<INSERT LOGO ASCII HERE />", "color: red;");
 
 console.log("%c~~~~~~~~~~~~~~~~~~~~~~~~~", "color: #BBC0C2;");
-
-var food = function(answer) {
-	if (answer === undefined) {
-		options = ["FINE THEN! No food for you!!!", "You want food too? YAY! :D"]
-		console.log("Do you want food? Answer with 'food(1)' if you do! Do 'food(0)' if you don't want some!")
-	} else {
-		console.log(options[answer]);
-	}
-
-};
