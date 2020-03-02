f399b849f53ec0a64b9f96580f9f12e66f5d1650
dmynerd78 <dmynerd78@users.noreply.github.com>
2020-03-01 17:57:54 -0700

    Revamp site internals to be more lightweight

--- js/copyright.js
+++
@@ -1,6 +0,0 @@
-console.log("%c~~~~~~~~~~~~~~~~~~~~~~~~~", "color: #BBC0C2;");
-
-console.log("%c(c) dmynerd78", "color: #FB9C10;");
-console.log("%c<INSERT LOGO ASCII HERE />", "color: red;");
-
-console.log("%c~~~~~~~~~~~~~~~~~~~~~~~~~", "color: #BBC0C2;");
--- js/newScreen.js
+++
@@ -1,5 +0,0 @@
-var newScreen = function() {
-                var easterEgg = window.open("", "EasterEggWindow", "width=200, height=100");
-                easterEgg.document.write("<p>You found an easter egg! Literally!</p>" + 
-                "<img src='http://www.travelopulentbox.com/ghostwriter/wp-content/uploads/2015/03/Sky_Blue_Easter_Egg_with_Flowers_and_Yellow_Bow_PNG_Picture.png' width='50' />");
-}
