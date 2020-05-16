2da467210c3768c33852d0580603c2735c8c1990
dmynerd78 <dmynerd78@users.noreply.github.com>
2020-05-16 15:44:57 -0700

    Go to top button

---
+++ js/scroll.js
@@ -0,0 +1,31 @@
+function scrollTop() {
+    return window.pageYOffset || document.documentElement.scrollTop || document.body.scrollTop || 0;
+}
+
+function insertGoToTop() {
+    // TODO Fade in/out
+    let goToTop = document.createElement('template');
+    goToTop.innerHTML = `<div id="gototop">
+        <!-- Source: FontAwesome -->
+            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512">
+            <path d="M240.971 130.524l194.343 194.343c9.373 9.373 9.373 24.569 0 33.941l-22.667 22.667c-9.357 9.357-24.522 9.375-33.901.04L224 227.495 69.255 381.516c-9.379 9.335-24.544 9.317-33.901-.04l-22.667-22.667c-9.373-9.373-9.373-24.569 0-33.941L207.03 130.525c9.372-9.373 24.568-9.373 33.941-.001z" />
+        </svg>
+    </div>`.trim();
+    goToTop = goToTop.content.firstChild;
+
+    goToTop.onclick = function () {
+        window.scrollTo({ top: 0, behavior: "smooth" });
+    };
+
+    window.addEventListener("scroll", function () {
+        if (scrollTop() > 400) {
+            goToTop.style.display = "block";
+        } else {
+            goToTop.style.display = "none";
+        }
+    });
+
+    document.querySelector("#right").appendChild(goToTop);
+}
+
+insertGoToTop();
